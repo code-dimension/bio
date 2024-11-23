8a1ecf8ed61399f12598df2ae5ffb88cee7081bc
Henrique Custódia <martinshenrique8@gmail.com>
2024-11-23 16:51:45 -0300

    Delete dist directory

--- dist/bio/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-B6ZRIVIE.mjs";import"./chunk-VYJSWFR7.mjs";import"./chunk-4KZRSLMR.mjs";export{a as default};
--- dist/bio/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{Ua as o,Va as r,Wa as n,ya as e}from"./chunk-VYJSWFR7.mjs";import"./chunk-4KZRSLMR.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
